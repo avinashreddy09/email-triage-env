dafca372f5b4a74c0516f58299ff4969e2170c0b
= <=>
2026-04-03 11:25:52 +0530

    Fix project.scripts entry point

--- server/app.py
+++ server/app.py
@@ -1,7 +1,9 @@
 import sys
 import os
 
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# Add parent directory to path
+parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, parent_dir)
 
 from main import app
 
